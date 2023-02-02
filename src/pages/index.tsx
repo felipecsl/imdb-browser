c02ad8346d25e3ff2881cdfa301678fde504ed70
Felipe Lima <felipe.lima@gmail.com>
2023-02-01 23:34:23 -0300

    moar minor cleanupg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,36 +18,37 @@ export default function Home() {
     }
   }, [query]);
   const onChangeSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length > 3) setQuery(e.target.value);
+    const value = e.target.value;
+    if (value.length > 3) setQuery(value);
   };
   return (
     <>
       <Head>
         <title>Create Next App</title>
-        <meta name="description" content="Julipe movie db" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="IMDB search"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
       <main className="m-8">
-        <h1 className="text-3xl my-4 font-medium">IMDB title search</h1>
+        <h1 className="text-3xl my-4 font-medium">IMDB search</h1>
         <input
           type="text"
           placeholder="Search..."
           onChange={debounce(onChangeSearchQuery, 250)}
           className="border border-gray-600 rounded py-2 px-4 my-2 text-xl"
         />
-        <br />
+        <br/>
         {results.length > 0 && (
           <div className="my-2">
             <p className="mb-3">{totalMatches} matches found</p>
             <div className="">
-              {results.map((record) => (
-                <div key={record.tconst} className="my-1">
+              {results.map(({ tconst, originalTitle, startYear }) => (
+                <div key={tconst} className="my-1">
                   <a
-                    href={`https://imdb.com/title/${record.tconst}`}
+                    href={`https://imdb.com/title/${tconst}`}
                     className="underline"
                   >
-                    {record.originalTitle.trim()}&nbsp;({record.startYear})
+                    {originalTitle.trim()}&nbsp;({startYear})
                   </a>
                 </div>
               ))}
